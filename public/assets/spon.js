5708d5675bbf3df162465dda513f3002373adadb
coleh <coleh@coleh.net>
2021-09-06 12:14:45 -0400

    Better proxy to avoid false positive

--- public/assets/spon.js
+++ public/assets/spon.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
     var MODAL_HTML = `
     <strong>This ad does not use your information.</strong>
     <br>All ads on Dinner Coding Time are context-based, reviewed by humans and controlled by first-party systems. 
@@ -6,23 +6,23 @@
     <br>If this ad is inappropriate to you, please <a href="/report-bug">report it</a>
     `;
 
-    window.addEventListener("load", window.scanSpons = function() {
+    window.addEventListener("load", window.scanSpons = function () {
         var spons = Array.from(document.getElementsByClassName("spon"));
-        if(spons.length == 0) {
+        if (spons.length == 0) {
             spons = [addDynamicContainer()];
         }
 
-        for(var i = spons.length - 1; i >= 0; i--) {
+        for (var i = spons.length - 1; i >= 0; i--) {
             var spon = spons[i];
-            if(spon.hasAttribute("data-spon-modal-added")) continue;
+            if (spon.hasAttribute("data-spon-modal-added")) continue;
             spon.setAttribute("data-spon-modal-added", "true");
             spon.appendChild(makeSponInfoButton());
         }
     });
 
-    window.__reloadSpons = function() {
+    window.__reloadSpons = function () {
         var spons = Array.from(document.getElementsByClassName("spontent"));
-        for(var i = spons.length - 1; i >= 0; i--) {
+        for (var i = spons.length - 1; i >= 0; i--) {
             spons[i].innerHTML = "";
             getDynamicFill(spons[i]);
         }
@@ -30,12 +30,12 @@
 
     function makeSponInfoButton() {
         var button = document.createElement("button");
-        button.setAttribute("aria-label","Sponsor Information")
+        button.setAttribute("aria-label", "Sponsor Information")
         button.innerHTML = `<svg viewBox="0 0 33.866666 33.866668"><path d="m 16.93385,2.7124717 c -7.8422346,0 -14.2208373,6.3785976 -14.2208373,14.2208373 0,7.842231 6.3786027,14.22032 14.2208373,14.22032 7.842236,0 14.220322,-6.378089 14.220322,-14.22032 0,-7.8422397 -6.378086,-14.2208373 -14.220322,-14.2208373 z m 0,2.0251994 c 6.746921,0 12.195122,5.4487179 12.195122,12.1956379 0,6.746921 -5.448201,12.195123 -12.195122,12.195123 -6.74692,0 -12.1956376,-5.448202 -12.1956378,-12.195123 0,-6.74692 5.4487178,-12.1956379 12.1956378,-12.1956379 z m 0.151413,3.1827516 c -0.49539,0 -0.885764,0.1354466 -1.170988,0.40566 -0.285224,0.2551999 -0.427881,0.60798 -0.427881,1.0583333 0,0.4353468 0.142657,0.780396 0.427881,1.035596 0.285224,0.2552 0.675598,0.382922 1.170988,0.382922 0.49539,0 0.885763,-0.127722 1.170987,-0.382922 0.285225,-0.2552 0.427881,-0.6002492 0.427881,-1.035596 0,-0.4503533 -0.142656,-0.8031334 -0.427881,-1.0583333 -0.285224,-0.2702134 -0.675597,-0.40566 -1.170987,-0.40566 z m -4.616256,5.1118323 v 1.846399 h 3.82819 v 8.691977 H 11.90625 V 25.41703 H 22.489584 V 23.570631 H 18.323946 V 13.032255 Z"></svg>`
         button.classList.add("sponfo");
         button.setAttribute("tabindex", "0");
 
-        button.addEventListener("click", function() {
+        button.addEventListener("click", function () {
             var rect = button.getClientRects()[0];
             var x = window.scrollX + rect.x + (rect.width / 2);
             var y = window.scrollY + rect.y + (rect.height / 2);
@@ -58,10 +58,10 @@
         elem.focus();
 
         var diaBox = elem.getClientRects()[0];
-        if(diaBox.y + diaBox.height >= window.innerHeight) {
+        if (diaBox.y + diaBox.height >= window.innerHeight) {
             elem.style.top = (y - (diaBox.y + diaBox.height - window.innerHeight)) + "px";
         }
-        if(diaBox.x + diaBox.width >= window.innerWidth) {
+        if (diaBox.x + diaBox.width >= window.innerWidth) {
             elem.style.left = (x - (diaBox.x + diaBox.width - window.innerWidth)) + "px";
         }
 
@@ -70,8 +70,8 @@
         focusme.setAttribute("tabindex", 0);
         document.body.appendChild(focusme);
 
-        elem.addEventListener("focusout", function(event) {
-            if(!event.relatedTarget || event.relatedTarget.parentElement != elem) {
+        elem.addEventListener("focusout", function (event) {
+            if (!event.relatedTarget || event.relatedTarget.parentElement != elem) {
                 button.focus();
                 document.body.removeChild(elem);
                 document.body.removeChild(focusme);
@@ -89,7 +89,7 @@
         container.appendChild(spontent);
 
         var footer = document.getElementsByTagName("footer")[0];
-        if(footer) {
+        if (footer) {
             footer.parentElement.insertBefore(container, footer);
         }
 
@@ -100,7 +100,7 @@
         var url = downstreamUrl || "https://www.videosprofitnetwork.com/watch.xml?key=4753ca5f9676cce88d784b428c765f00";
         var xhr = new XMLHttpRequest();
         xhr.open("GET", proxify(url));
-        xhr.onload = function() {
+        xhr.onload = function () {
             var domParser = new DOMParser();
             var xml = domParser.parseFromString(xhr.responseText, "text/xml");
             renderDynamicFill(parent, xml);
@@ -109,33 +109,33 @@
     }
 
     function renderDynamicFill(parent, xml) {
-        if(getTagElementContent(xml, "Impression")) {
+        if (getTagElementContent(xml, "Impression")) {
             sendCallback(xml.getElementsByTagName("Impression")[0].textContent);
         }
         var link = document.createElement("a");
         link.style.display = "block";
         link.setAttribute("target", "_blank");
         link.setAttribute("rel", "sponsored noopener");
-        
+
         link.setAttribute("href", proxify(getTagElementContent(xml, "ClickThrough")));
-        if(link.getAttribute("href") == "") link.removeAttribute("href");
-        
-        
+        if (link.getAttribute("href") == "") link.removeAttribute("href");
+
+
         parent.appendChild(link);
 
-        if(getTagElementContent(xml, "ClickTracking")) {
-            link.addEventListener("click", function() {
+        if (getTagElementContent(xml, "ClickTracking")) {
+            link.addEventListener("click", function () {
                 sendCallback(getTagElementContent(xml, "ClickTracking"));
             });
         }
 
         //child ads
-        if(getTagElementContent(xml, "VASTAdTagURI")) {
+        if (getTagElementContent(xml, "VASTAdTagURI")) {
             getDynamicFill(link, getTagElementContent(xml, "VASTAdTagURI"));
         } else {
             //ad title
             var adTitle = getTagElementContent(xml, "AdTitle");
-            if(adTitle) {
+            if (adTitle) {
                 var titleElem = document.createElement("h2");
                 titleElem.setAttribute("style", 'margin:0;font-size:1em');
                 titleElem.textContent = adTitle;
@@ -145,7 +145,7 @@
             //ad parameters. If it's one of the formats we know, just handle it ourselves
             var adParams = getTagElementContent(xml, "AdParameters");
             var adParamsJson = JSON.parse(adParams);
-            if(adParamsJson.video_id) {
+            if (adParamsJson.video_id) {
                 var iframe = document.createElement("iframe");
                 iframe.src = generateYoutubeEmbedUrl(adParamsJson.video_id);
                 iframe.setAttribute("style", 'display:block;margin:auto;height:200px');
@@ -156,8 +156,8 @@
     }
 
     function generateYoutubeEmbedUrl(videoId) {
-        return "https://youtube.com/embed/" + videoId 
-        + "?origin=" + encodeURIComponent(window.location.origin);
+        return "https://youtube.com/embed/" + videoId
+            + "?origin=" + encodeURIComponent(window.location.origin);
     }
 
     function getTagElementContent(xml, tag) {
@@ -171,10 +171,19 @@
     }
 
     function proxify(url) {
-        if(!url || !url.startsWith) return url;
+        if (!url || !url.startsWith) return url;
 
-        if(url.startsWith("https://www.videosprofitnetwork.com")) return url.replace("https://www.videosprofitnetwork.com", "/proxy/vpnetwork");
-        else if(url.startsWith("https://cdn.cloudvideosa.com")) return url.replace("https://cdn.cloudvideosa.com", "/proxy/cloudvideocdn");
-        else return url;
+        if (url.startsWith("https://www.videosprofitnetwork.com/watch.xml")) {
+            return url.replace("https://www.videosprofitnetwork.com/watch.xml", "/proxy/vpnetwork/watch");
+        }
+        else if (url.startsWith("https://www.videosprofitnetwork.com/impr.gif?sid=")) {
+            return url.replace("https://www.videosprofitnetwork.com/impr.gif?sid=", "/proxy/vpnetwork/view?dat=");
+        }
+        else if (url.startsWith("https://cdn.cloudvideosa.com")) {
+            return url.replace("https://cdn.cloudvideosa.com", "/proxy/cloudvideocdn");
+        }
+        else {
+            return url;
+        }
     }
 })();
